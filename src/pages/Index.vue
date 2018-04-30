edc96f44944f7548c66e631718497c3cb7c3e00a
rmellema <rene.mellema@gmail.com>
2018-04-30 10:40:14 +0200

    Added some introductory text

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,14 @@
 <template>
   <MainLayout>
-    <p>Hello, world!</p>
+    <p>For the course Multi Agent Systems the RuG has developed a webapp to see
+    applications of Epistemic Logic in the area of communication protocols. The
+    webapp visualises the execution of three protocols, the two protocols from
+    the book "Epistemic Logic for AI and Computer Science" from W. van der Hoek
+    and J.J.Ch. Meyer, and the Transmission Control Protocol (TCP). All these
+    protocols can be reached by using the buttons in the navigation bar at the
+    top. The visualizations for the protocols from the book also allow you to
+    experiment with the rules in the protocols. This makes it possible to see
+    why all the rules are necessary.</p>
   </MainLayout>
 </template>
 
@@ -15,3 +23,11 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+p {
+  width: 80%;
+  margin-left: 10%;
+  margin-right: 10%;
+}
+</style>
